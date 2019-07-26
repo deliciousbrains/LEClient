c64b24fa9c4a239bc7633c279961b75e0cd8b036
Ashley Rich <hello@ashleyrich.com>
2019-07-26 10:06:45 +0100

    IDs have been removed from LE response

--- src/LEAccount.php
+++ src/LEAccount.php
@@ -129,7 +129,6 @@ class LEAccount
 		$post = $this->connector->post($this->connector->accountURL, $sign);
 		if(strpos($post['header'], "200 OK") !== false)
 		{
-			$this->id = $post['body']['id'];
 			$this->key = $post['body']['key'];
 			$this->contact = $post['body']['contact'];
 			$this->agreement = isset($post['body']['agreement']) ? $post['body']['agreement'] : '';
@@ -158,7 +157,6 @@ class LEAccount
 		$post = $this->connector->post($this->connector->accountURL, $sign);
 		if(strpos($post['header'], "200 OK") !== false)
 		{
-			$this->id = $post['body']['id'];
 			$this->key = $post['body']['key'];
 			$this->contact = $post['body']['contact'];
 			$this->agreement = isset($post['body']['agreement']) ? $post['body']['agreement'] : '';
